abcdb2bf06b6e8ecd1c079f90dcfbe9f5aefe19a
Bainbridge  Lewis <M2003344@middlesbro.ac.uk>
2020-12-09 16:21:47 +0000

    first commit

---
+++ src/poggers/Person.java
@@ -0,0 +1,6 @@
+
+package poggers;
+
+public class Person {
+    
+}
---
+++ src/poggers/Poggers.java
@@ -0,0 +1,32 @@
+
+package poggers;
+import java.util.Scanner;
+public class Poggers {
+    private static String fName;
+    private static String lName;
+    private static String Initial;
+    
+    
+    
+    public static void main(String[] args){
+        fName = "Reis";
+        lName = "Mcalister";
+        Initial = "RM";
+    }
+     public static void logincheck(){
+         String uName = "Highlander";
+         String pWord = "SCOTLAND";
+     
+         if ((uName.equals("Highlander")) && (pWord.equals("SCOTLAND"))){
+         System.out.println("Welcome");
+     }else{
+             System.out.println("Incorrect");
+             }
+     }
+        
+   
+
+    }
+
+    
+
